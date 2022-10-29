2041655597ea0fcba062f67a8c28a3972f28cca3
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-10-29 16:55:37 -0300

    feat: Inserir todos os funcionários na lista funcionarios

--- Desafio_Projedata/src/Funcionario.java
+++ Desafio_Projedata/src/Funcionario.java
@@ -3,14 +3,9 @@ import java.time.format.DateTimeFormatter;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
     private BigDecimal salario;
     private String funcao;
-
-    public Funcionario(BigDecimal salario, String funcao) {
-        this.salario = salario;
-        this.funcao = funcao;
-    }
 
     public Funcionario(String _nome, CharSequence _data, BigDecimal salario, String funcao) {
         super(_nome, _data);
@@ -32,5 +27,13 @@ public class Funcionario {
 
     public void setFuncao(String funcao) {
         this.funcao = funcao;
+    }
+    @Override
+    public String toString() {
+        Locale localeBR = new Locale("pt","BR");
+        NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
+        return "[Nome: " + getNome() + ", Nascimento: "
+                + getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM/uuuu")) + ", Salario: " 
+                + dinheiro.format(this.salario) + ", Função: " + this.funcao + "]";
     }
 }
